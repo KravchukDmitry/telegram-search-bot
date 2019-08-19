a5aef0f70a9beae772ad9e1e48f87c4f6cb540fe
Dmitry <kravchukdim@gmail.comheroku>
2019-08-19 23:20:28 +0300

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.net.Proxy;
 
 public class Main {
     public static void main(String[] args) {
